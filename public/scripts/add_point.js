dbdf53c2698c02c9d4bcfba19ab3c31e8fc9c34b
Eliel504 <julia3192016@outlook.com>
2021-02-18 09:56:35 -0300

    Adicionando condições para realizar a contagem de pontos

--- public/scripts/add_point.js
+++ public/scripts/add_point.js
@@ -10,63 +10,99 @@ var correct_home = 0;
 //AWAY-----------------------------
 
 add_point_tree_away= function(){
-    cont_away += 3;
-    correct_away = 3;
+    if(valid_play_addPoint){
+        cont_away += 3;
+        correct_away = 3;
+    
+        placar_away.innerHTML= cont_away;
+    }else{
+        alert('a partida não foi iniciada')
+    }
 
-    placar_away.innerHTML= cont_away;
 }
 
 add_point_two_away = function(){
-    cont_away += 2;
-    correct_away = 2;
-
-    placar_away.innerHTML= cont_away;
+    if(valid_play_addPoint){
+        cont_away += 2;
+        correct_away = 2;
+    
+        placar_away.innerHTML= cont_away;
+    }else{
+        alert('a partida não foi iniciada')
+    }
 }
 
 add_point_one_away = function(){
-    cont_away += 1;
-    correct_away = 1;
-
-    placar_away.innerHTML= cont_away;
+    if(valid_play_addPoint){
+        cont_away += 1;
+        correct_away = 1;
+    
+        placar_away.innerHTML= cont_away;
+    }else{
+        alert('a partida não foi iniciada')
+    }
 }
 
 correct_point_away = function(){
-    cont_away -= correct_away;
-    placar_away.innerHTML= cont_away;
-    
-    correct_away = 0;
+    if(valid_play_addPoint){
+        cont_away -= correct_away;
+        placar_away.innerHTML= cont_away;
+        
+        correct_away = 0;
+    }else{
+        alert('a partida não foi iniciada')
+    }
+
 }
 
 //HOME------------------------------------
 
 add_point_tree_home= function(){
-    cont_home += 3;
-    correct_home = 3;
+    if(valid_play_addPoint){
+        cont_home += 3;
+        correct_home = 3;
+    
+        placar_home.innerHTML= cont_home;
+    }else{
+        alert('a partida não foi iniciada')
+    }
 
-    placar_home.innerHTML= cont_home;
 
 
 }
 
 add_point_two_home = function(){
-    cont_home += 2;
-    correct_home = 2;
-
-    placar_home.innerHTML= cont_home;
+    if(valid_play_addPoint){
+        cont_home += 2;
+        correct_home = 2;
+    
+        placar_home.innerHTML= cont_home;
+    }else{
+        alert('a partida não foi iniciada')
+    }
 
 }
 
 add_point_one_home = function(){
-    cont_home += 1;
-    correct_home = 1;
-
-    placar_home.innerHTML= cont_home;
+    if(valid_play_addPoint){
+        cont_home += 1;
+        correct_home = 1;
+    
+        placar_home.innerHTML= cont_home;
+    }else{
+        alert('a partida não foi iniciada')
+    };
 
 }
 
 correct_point_home = function(){
-    cont_home -= correct_home;
+    if(valid_play_addPoint){
+        cont_home -= correct_home;
 
-    placar_home.innerHTML= cont_home;
-    correct_home = 0;
+        placar_home.innerHTML= cont_home;
+        correct_home = 0;
+    }else{
+        alert('a partida não foi iniciada') 
+    }
+
 }
